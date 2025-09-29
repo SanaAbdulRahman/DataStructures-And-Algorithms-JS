04bfd87d1eb3db60c4e052fd49d4cebfff214c04
SanaAbdulRahman <92288840+SanaAbdulRahman@users.noreply.github.com>
2025-09-29 19:27:02 +0400

    Time: 0 ms (100.00%) | Memory: 56.3 MB (29.75%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+    var twoSum = function(nums, target) {
+    const numMap=new Map();
+    for(let i=0;i<nums.length;i++){
+        const diff=target-nums[i];
+        if(numMap.has(diff)){
+            return [numMap.get(diff),i]
+        }
+        numMap.set(nums[i],i);
+    }
+    };
