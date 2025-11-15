a506aa51cd1149e010686b0bf3e3dcb76141e28f
SanaAbdulRahman <92288840+SanaAbdulRahman@users.noreply.github.com>
2025-11-15 17:39:33 +0400

    Time: 7 ms (61.28%) | Memory: 58 MB (51.36%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxFreqSum = function(s) {
+    let map={}; //declare a hash map
+    for(let i=0;i<s.length;i++){
+        if(!map[s[i]]){
+            map[s[i]]=1
+        }
+        else{
+            ++map[s[i]];
+        }
+    }
+    let vowels=['a','e','i','o','u'];
+    let maxVowel=0 ;
+    let maxConsonant=0;
+
+    for(let i=0;i<s.length;i++) {
+        if(vowels.includes(s[i])){
+            if(map[s[i]]>maxVowel){
+                maxVowel=map[s[i]];
+            }
+        }else
+        {
+            if(map[s[i]]>maxConsonant){
+                maxConsonant=map[s[i]];
+            }
+        }
+    }
+    return maxVowel+maxConsonant;
+};
