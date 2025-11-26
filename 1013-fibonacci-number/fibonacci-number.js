e48d553c4177a7abc862705c4a9beb69dbc9eedc
SanaAbdulRahman <92288840+SanaAbdulRahman@users.noreply.github.com>
2025-11-26 18:01:53 +0400

    Time: 38 ms (94.32%) | Memory: 55.2 MB (8.05%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n<2)return n
+
+let prev1=1;
+let prev2=0;
+
+    for(let i=2;i<=n;i++){
+
+        let curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+    }
+    return prev1;
+};
