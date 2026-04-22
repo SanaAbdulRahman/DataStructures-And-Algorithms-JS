ef868e5b826e490d46c80c290126681d93e290f4
SanaAbdulRahman <92288840+SanaAbdulRahman@users.noreply.github.com>
2026-04-22 10:56:57 +0400

    Time: 39 ms (16.61%) | Memory: 64.1 MB (5.05%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,13 +3,27 @@
  * @param {number} target
  * @return {number[]}
  */
-    var twoSum = function(nums, target) {
-    const numMap=new Map();
-    for(let i=0;i<nums.length;i++){
-        const diff=target-nums[i];
-        if(numMap.has(diff)){
-            return [numMap.get(diff),i]
-        }
-        numMap.set(nums[i],i);
+var twoSum = function(nums, target) {
+    /*sort the array and use 2 pointer method but return the exact indices of the original array*/
+    var array=nums.map((num,index)=>[num,index]);
+console.log("array",array);
+    array.sort((a,b)=>a[0]-b[0]);
+    console.log("sorted array",array);
+
+    let left=0;
+    let right=nums.length-1
+    while(left<right){
+    
+    let sum=array[left][0]+array[right][0];
+
+    if(sum===target){
+        return [array[left][1],array[right][1]];
     }
-    };
+    else if(sum<target)
+    left++;
+
+    else if(sum>target)
+    right--
+    }
+return []
+};
